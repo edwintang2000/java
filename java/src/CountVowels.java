addd58d6a888c8f3d10eae4680f62198d2455d88
eq_ta <eq_ta@DESKTOP-vivian.dev1.nciids.local>
2019-04-22 00:03:54 -0400

    CountVowels class

---
+++ java/src/CountVowels.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class CountVowels {
+
+	public CountVowels() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please input a sentence:");
+		String str = in.nextLine();
+		str = str.toLowerCase();	
+		in.close();
+		int vowels = 0;
+		int consonants=0;
+		for (int i = 0 ; i < str.length(); i++) {
+			if ((str.charAt(i) == 'a') || (str.charAt(i) == 'e') || (str.charAt(i) == 'i') || (str.charAt(i) == 'o') || (str.charAt(i) == 'u') || (str.charAt(i) == 'y')) {
+				vowels++;
+			}
+			else if (str.charAt(i) != ' ') {
+				consonants++;
+			}
+		}
+		System.out.println("There are " + vowels + " vowels");
+		System.out.println("There are " + consonants + " consonants");
+	}
+
+}
